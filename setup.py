005d39635f8a1eaea603ea796c5ec4b43f85cdf9
Scott White <whiteau@fhcrc.org>
2019-07-16 11:32:17 -0700

    Added setup.py to track package dependencies

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='RelapseDataReformatting',
+    version='0.1',
+    packages=['pycap',
+              'pandas',
+              'pandas-profiling'],
+    url='https://github.com/FredHutch/RelapseDataReformatting',
+    license='Apache 2.0',
+    author='Fred Hutch HDC',
+    author_email='',
+    description='A reformatting package for the Relapse project, targetting the RETAIN model'
+)
