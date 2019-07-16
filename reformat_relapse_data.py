14c64fb13bae2e453473460acdbab506e0332f70
Scott White <whiteau@fhcrc.org>
2019-07-16 11:28:28 -0700

    reformat_relapse_data now generates pandas profiling pages

--- reformat_relapse_data.py
+++ reformat_relapse_data.py
@@ -1,6 +1,7 @@
 import json
 import os
 import pandas
+import pandas_profiling
 
 from redcap import Project, RedcapError
 
@@ -13,14 +14,21 @@ def pull_from_red_cap(config):
     API_KEY = config["RED_CAP_ENGINE"]["API"]
     project = Project(URL, API_KEY)
 
-    try:
-        intermediate_df = project.export_records(format='df')
-    except RedcapError:
-        print ("Failure to export records from REDCap")
+    for form in project.forms:
+        try:
+            intermediate_df = project.export_records(forms=[form], format='df')
+        except RedcapError:
+            print ("Failure to export records from REDCap for form: {}".format(form))
+            continue
 
-    print (str(len(intermediate_df) )+ ' total records pulled')
-    intermediate_df.to_csv(path_or_buf=(config['OUTPUT_FILEPATH'] + os.path.sep + 'intermediate_df.csv'))
-    
+        intermediate_df.isna().sum()
+        length = intermediate_df.shape[0]
+        print("Empty columns:")
+        empty_cols = [col for col in list(intermediate_df) if intermediate_df[col].isna().sum() == length]
+        print("length, width {}, number of empty columns: {}".format(intermediate_df.shape, len(empty_cols)))
+
+        report = pandas_profiling.ProfileReport(intermediate_df)
+        report.to_file(os.path.sep.join([config["OUTPUT_FILEPATH"], "{}.html".format(form)]))
 
 if __name__ == '__main__':
     with open('./config.json') as fin:
