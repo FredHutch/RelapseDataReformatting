0c7e4bbf1980b5dd110458af74732e052c7387b3
ddong63 <ddong@fredhutch.org>
2019-08-05 16:15:36 -0700

    map feature names to integers
    - map feature names to integers
    - created categorical_feature.csv (saved in DS share folder /data_dict)

--- scripts/map_categorical_features.py
+++ scripts/map_categorical_features.py
@@ -2,7 +2,7 @@
 create a data dictionary that maps distinct categorical or 'code' features
 to unique integers for retain model training and interpretation
 """
-
+import os
 import csv
 import pickle
 
@@ -13,18 +13,16 @@ class DataDictionary():
         self.numeric_cols = []
         self.drop_cols = []
         if default_datadict:
-            self.read_data_dict('./data_dictionary.csv')
+            self.read_data_dict(os.path.realpath('data_dict/data_dictionary.csv'))
         self.code_mappings = {}
         if default_codes:
-            self.read_code_mapping('./dictionary.pkl')
+            self.read_code_mapping(os.path.realpath('data_dict/data_dictionary.pkl'))
 
     def read_data_dict(self, filepath):
         with open(filepath, "r") as fin:
             reader = csv.DictReader(fin)
             for row in reader:
-                if (row['DataTreatment'] == 'Code' and row['OneHotEncoded'] == 'Yes'):
-                    self.one_hot_encoded[row['Name']] = (row['DataTreatment'], row['DataType'], row['Temporal'], row['OneHotEncoded'])
-                elif (row['DataTreatment'] == 'Code' and row['OneHotEncoded'] == 'No'):
+                if (row['DataTreatment'] == 'Code'):
                     self.code_cols[row['Name']] = (row['DataTreatment'], row['DataType'], row['Temporal'], row['OneHotEncoded'])
                 elif (row['DataTreatment'] == 'Numeric'):
                     self.numeric_cols.append(row['Name'])
@@ -37,4 +35,31 @@ class DataDictionary():
         self.code_mappings = {v:k for k,v in code_map.items()}
 
 
-## to-do map code and save dict to a pickle
+
+def map_codes_to_ints(filepath):
+    '''
+    map one hot encoded features (binary) to integer feature names
+    dump it to a pickle file
+    :param filepath: path of data_dictionary.csv
+    :return: pickle file with one hot encoded features,
+             e,g, {1: feature1, 2: feature2, ...}
+    '''
+    code_d = {}
+    int_map_count = 1
+    with open(filepath, "r") as fin:
+        reader = csv.DictReader(fin)
+        for row in reader:
+            if (row['DataTreatment'] == 'Code'):
+                code_d[int_map_count] = row['Name']
+                int_map_count += 1
+    with open(os.path.realpath('data_dict/data_dictionary.pkl'), 'wb') as code_map:
+        pickle.dump(code_d, code_map)
+
+def one_hot_encoding(categorical_features):
+    '''
+    To-do: one-hot encoding features while load in the data
+    :param categorical_features:
+    :return:
+    '''
+    # load categorical_features.csv
+    # one hot encoding the features
