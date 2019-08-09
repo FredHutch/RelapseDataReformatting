e657264d982d8871a7748f0eb0287d3e2f3751fe
Silgard <esilgard@fredhutch.org>
2019-08-08 17:22:26 -0700

    pass single dataframe rows to event factories

--- scripts/reformat_relapse_data.py
+++ scripts/reformat_relapse_data.py
@@ -22,26 +22,15 @@ def pull_from_red_cap(config):
     URL = config["RED_CAP_ENGINE"]["URL"]
     API_KEY = config["RED_CAP_ENGINE"]["API"]
     project = Project(URL, API_KEY)
-    form_df = dict()
-
     timelines = defaultdict(dict)
+
     for form in project.forms:
         try:
             intermediate_df = project.export_records(forms=[form], format='df')
-
-            '''
-            vital status and person_id information is included by default in most instruments
-            and appears as an extra row with *mostly* empty columns
-            we are dropping it for profiling purposes since it throws the counts/correlations off.
-            '''
-            form_df[form] = intermediate_df
-            if form not in NON_REPEAT_FORMS:
-                form_df[form] = intermediate_df.loc[intermediate_df['redcap_repeat_instrument'].notnull()]
         except RedcapError:
             print("Failure to export records from REDCap for form: {}".format(form))
             continue
-
-        events = map_instrument_df_to_class(form_df[form])
+        events = map_instrument_df_to_class(form, intermediate_df)
 
         for event in events:
             if event.date not in timelines[event.patientid].keys():
@@ -50,5 +39,7 @@ def pull_from_red_cap(config):
                 timelines[event.patientid][event.date].add_event(event)
 
 
-
-
+if __name__ == '__main__':
+    with open('../config.json') as fin:
+        config = json.load(fin)
+    pull_from_red_cap(config)
