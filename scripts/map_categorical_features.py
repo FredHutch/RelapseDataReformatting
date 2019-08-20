f012576d1e8a8d824604db8be3c3ab9de5aa7ea5
Dora Dong <39133175+ddong63@users.noreply.github.com>
2019-08-20 12:16:36 -0700

    Data dict (#8)
    * initial commit: load data dictionary
    
    * map feature names to integers
    
    - map feature names to integers
    - created categorical_feature.csv (saved in DS share folder /data_dict)
    
    * add one hot encoding function
    
    - cleaned up path to be global param
    - one hot encoding for each feature
    - To-do: match data dictionay, drop features.
    
    * add feature recode map for gateway data
    
    * updated proplbl category mapping

---
+++ scripts/map_categorical_features.py
@@ -0,0 +1,125 @@
+"""
+create a data dictionary that maps distinct categorical or 'code' features
+to unique integers for retain model training and interpretation
+"""
+import os
+import csv
+import pickle
+
+# these file paths will be move to a common file eventually
+data_dict_pkl_path = os.path.realpath('data_dict/data_dictionary.pkl')
+data_dict_csv_path = os.path.realpath('data_dict/data_dictionary.csv')
+categorical_feature_path = os.path.realpath('data_dict/categorical_features.csv')
+
+# Note: cmvx: '-/' Krakow is working on QA this field.
+# Other criteria:
+#   ## Create:
+#             - MaleDonor_Female_Recipient: if sex == 'Male' and donsex == 'Female', then 1; else: 0
+#             - Age vs. treatment count
+#             - Time to first relapse
+#             - % MRD vs. morph
+
+gateway_feature_recode_map = {'cmvx': {'-/-': 'neg_neg', '-/': 'neg_neg', '/': 'Unknown',
+                                       '+/-': 'Others', '+/+': 'Others', '-/+': 'Others',
+                                       '+/': 'Others', '-/+': 'Others'},
+                              'tbidose': {'0': 'le450', '200': 'le450', '300': 'le450', '450': 'le450',
+                                          '1200': 'ge1200', '1320': 'ge1200'},
+                              'proplbl': {
+                                  # contains full list of the proplbl
+                                  'FK506': 'CNI',
+                                  'NEORAL': 'CNI',
+                                  'CSP': 'CNI',
+                                  'CYCLOSPORINE': 'CNI',
+                                  'CSA': 'CNI',
+                                  'TACROLIMUS' : 'CNI',
+                                  'RAPA': 'RAPA',
+                                  'SIROLIUMS': 'RAPA',
+                                  'MMF': 'MMF',
+                                  '[MYCOPHENOLATESODIUM]': 'MMF',
+                                  'CY': 'Post-Transplant_Cyclophosphamide',
+                                  'PTCY': 'Post-Transplant_Cyclophosphamide',
+                                  'STEROIDS': '', # ignore STEROIDS
+                                  'MTX': 'MTX'
+                              },
+                              'hla_cco': {'ISO/MATCHED': 'REL/MATCHED',
+                                          'REL/MATCHED': 'REL/MATCHED',
+                                          'REL/MISMATCH': 'REL/MISMATCH',
+                                          'REL/HAPLOIDENTICAL': 'REL/HAPLOIDENTICAL',
+                                          'REL/UNKNOWN': '',
+                                          'RD/CORD': 'CORD',
+                                          'URD/CORD-COMBINED': 'CORD',
+                                          'URD/MATCHED': 'URD/MATCHED',
+                                          'URD/MISMATCH': 'URD/MISMATCH'}}
+
+gateway_feature_to_integer = {'cmvx': {'neg_neg': 1, 'Others': 2, 'Unknown': 3},
+                              'proplbl': {'CNI': 1, 'RAPA': 2, 'MMF': 3, 'CY': 4, 'MTX': 5},
+                              'hla_cco': {'REL/MATCHED': 1, 'REL/MISMATCH': 2, 'REL/HAPLOIDENTICAL': 3,
+                                          'RD/CORD': 4, 'URD/MATCHED': 5, 'URD/MISMATCH': 6},
+                              'tbidose': {'le450': 1, 'ge1200': 2},
+                              'celltxl': {'PBSC': 1, 'BM': 2, 'CORD': 3}
+                              }
+
+class DataDictionary():
+    def __init__(self, default_datadict=None, default_codes=None):
+        self.code_cols = {}
+        self.numeric_cols = []
+        self.drop_cols = []
+        self.code_mappings = {}
+        self.read_data_dict(data_dict_csv_path)
+        self.read_code_mapping(data_dict_pkl_path)
+
+    def read_data_dict(self, filepath):
+        with open(filepath, "r") as fin:
+            reader = csv.DictReader(fin)
+            for row in reader:
+                if (row['DataTreatment'] == 'Code'):
+                    self.code_cols[row['Name']] = (row['DataTreatment'], row['DataType'], row['Temporal'], row['OneHotEncoded'])
+                elif (row['DataTreatment'] == 'Numeric'):
+                    self.numeric_cols.append(row['Name'])
+                else:
+                    self.drop_cols.append(row['Name'])
+
+    def read_code_mapping(self, filepath):
+        with open(filepath, "rb") as fin:
+            code_map = pickle.load(fin)
+        self.code_mappings = {v:k for k,v in code_map.items()}
+
+
+def map_codes_to_ints(datadict = data_dict_csv_path, outpath = data_dict_pkl_path):
+    '''
+    map one hot encoded features (binary) to integer feature names
+    dump it to a pickle file
+    :param filepath: path of data_dictionary.csv
+    :return: pickle file with one hot encoded features,
+             e,g, {1: feature1, 2: feature2, ...}
+    '''
+    code_d = {}
+    int_map_count = 1
+    with open(datadict, "r") as fin:
+        reader = csv.DictReader(fin)
+        for row in reader:
+            if (row['DataTreatment'] == 'Code'):
+                code_d[int_map_count] = row['Name']
+                int_map_count += 1
+    with open(outpath, 'wb') as code_map:
+        pickle.dump(code_d, code_map)
+
+def one_hot_encoding(feature, categorical_feature_mapping = categorical_feature_path):
+    '''
+    One-hot encoding, and rename categorical features
+    :feature: dict {name: val}, e.g., {feature: 3}
+    :categorical_feature_mapping: csv file with categorical features
+    :return: new feature name, e.g., feature_3
+    '''
+
+    # create dict for categorical features, {feature: n_total_levels}
+    cat_name = {}
+    with open(categorical_feature_mapping, "r") as fin:
+        reader = csv.DictReader(fin)
+        for row in reader:
+            cat_name[row['Name']] = row['N']
+
+    (name, val), = feature.items()
+
+    if cat_name.get(name):
+        return '{}_{}'.format(name, val)
