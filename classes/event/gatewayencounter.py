241d939b2c5e3dd4ba3904fd55d2d551696152af
Silgard <esilgard@fredhutch.org>
2019-08-21 13:24:02 -0700

    updated gateway days since epoch

--- classes/event/gatewayencounter.py
+++ classes/event/gatewayencounter.py
@@ -2,11 +2,9 @@ from classes.event.encounter import Encounter, EncounterFactory
 
 
 class gatewayEncounter(Encounter):
-    def __init__(self, patientid, date, days_since_epoch, days_since_relapse, **kwargs):
+    def __init__(self, patientid, date, days_since_epoch=0, **kwargs):
         super(gatewayEncounter, self).__init__(patientid, date, "gatewayEncounter")
         self.days_since_epoch = days_since_epoch
-        self.days_since_relapse = days_since_relapse
-        # to-do: get days_since_epoch and days_since_relapse from PatientTimeline
 
     def is_decision_point(self):
         """
