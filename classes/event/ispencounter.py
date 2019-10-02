b652326a7845a242ba6baa55ff1991a3b03abb08
Silgard <esilgard@fredhutch.org>
2019-10-02 16:34:05 -0700

    date censoring inline with other classes

--- classes/event/ispencounter.py
+++ classes/event/ispencounter.py
@@ -26,24 +26,19 @@ class ISPEncounter(Encounter):
         f['isp_event'] = self.isp_event
         return f
 
-
 class ISPEncounterFactory(EncounterFactory):
     def __init__(self):
         super(ISPEncounterFactory, self).__init__(ISPEncounter)
-
+        self.ISP_STOP_VAR = 2
     def translate_df_to_dict(self, df_row):
         row_dictionary = self._store_df_row(df_row)
-        row_dictionary['date'] = df_row.get('date_isp_action', None)
+        row_dictionary['date'] = Encounter._date_coercion(df_row['date_isp_action']) - \
+                                dt.timedelta(days=min(0,df_row.get('days_hct1_to_ispact')))
+        row_dictionary['days_since_epoch'] = max(0,df_row.get('days_hct1_to_ispact', 0))
         row_dictionary['patientid'] = df_row.get('subject_id', None)
-        row_dictionary['days_since_epoch'] = df_row.get('days_hct1_to_ispact', 0)
         row_dictionary['days_since_relapse'] = df_row.get('days_index_rel_to_ispact', None)
         row_dictionary['e_isp'] = df_row.get('e_isp', None)
         # do not encode ISP stop dates to look just like start and restart dates
-        if row_dictionary['e_isp'] != 2:
+        if row_dictionary['e_isp'] != self.ISP_STOP_VAR:
             row_dictionary['isp_event'] = 1
-        # censor ISP to be the HCT date at the earliest
-        if row_dictionary['days_since_epoch'] < 0:
-            row_dictionary['date'] = dt.datetime.strptime(row_dictionary['date'],'%Y-%m-%d') - \
-                                     dt.timedelta(days=row_dictionary['days_since_epoch'])
-            row_dictionary['days_since_epoch'] = 0
         return row_dictionary
