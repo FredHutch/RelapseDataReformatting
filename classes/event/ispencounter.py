2e08dc091af73797065b5cfcf297e3d39433cf65
Silgard <esilgard@fredhutch.org>
2019-10-03 09:54:33 -0700

    moving variable to ISPEncounter class

--- classes/event/ispencounter.py
+++ classes/event/ispencounter.py
@@ -3,6 +3,7 @@ from classes.event.encounter import Encounter, EncounterFactory
 
 
 class ISPEncounter(Encounter):
+    ISP_STOP_VAR = 2
     def __init__(self, patientid, date, days_since_epoch, days_since_relapse, **kwargs):
         super(ISPEncounter, self).__init__(patientid, date, days_since_epoch, days_since_relapse, **kwargs)
         self.e_isp = kwargs.get('e_isp', None)
@@ -29,7 +30,6 @@ class ISPEncounter(Encounter):
 class ISPEncounterFactory(EncounterFactory):
     def __init__(self):
         super(ISPEncounterFactory, self).__init__(ISPEncounter)
-        self.ISP_STOP_VAR = 2
     def translate_df_to_dict(self, df_row):
         row_dictionary = self._store_df_row(df_row)
         row_dictionary['date'] = Encounter._date_coercion(df_row['date_isp_action']) - \
@@ -39,6 +39,6 @@ class ISPEncounterFactory(EncounterFactory):
         row_dictionary['days_since_relapse'] = df_row.get('days_index_rel_to_ispact', None)
         row_dictionary['e_isp'] = df_row.get('e_isp', None)
         # do not encode ISP stop dates to look just like start and restart dates
-        if row_dictionary['e_isp'] != self.ISP_STOP_VAR:
+        if row_dictionary['e_isp'] != ISPEncounter.ISP_STOP_VAR:
             row_dictionary['isp_event'] = 1
         return row_dictionary
